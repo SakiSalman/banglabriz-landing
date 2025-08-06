319c4e909f5c756e6a309947fbbbe3e616f1df22
SakiSalman <codersaki98@gmail.com>
2025-08-07 00:43:28 +0600

    updated

--- views/Header/Header.tsx
+++ views/Header/Header.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { cn } from '@/utils/cn';
 import { MotionDiv } from '@/utils/motion.utils';
 import { Menu, Phone, X } from 'lucide-react';
 import Image from 'next/image';
@@ -9,7 +10,12 @@ const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <nav className="fixed w-full top-3 left-0 right-0 z-50">
-      <MotionDiv className="md:max-w-6xl mx-auto px-4 md:px-8 bg-[#ffffffe0] rounded-full ring ring-blue-900 shadow-lg">
+      <MotionDiv
+        className={cn(
+          'md:max-w-6xl mx-auto px-4 md:px-8 bg-[#ffffffe0] ring ring-blue-900 shadow-lg',
+          isMenuOpen ? 'rounded-lg' : 'rounded-full',
+        )}
+      >
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href={'/'} className="text-2xl font-bold text-primary">
@@ -105,13 +111,13 @@ const Header = () => {
                 </summary>
                 <div className="ml-4 mt-2 space-y-2">
                   <Link
-                    href="/services/student"
+                    href="/student-services"
                     className="block text-sm text-gray-700 hover:text-secondary"
                   >
                     Student Services
                   </Link>
                   <Link
-                    href="/services/worker"
+                    href="/worker-services"
                     className="block text-sm text-gray-700 hover:text-secondary"
                   >
                     Worker Services
